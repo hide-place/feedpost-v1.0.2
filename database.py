cb6e576c9c54ba67b7ed9a0a99700f2f6e9dad0e
hide-place <hideplace865@gmail.com>
2024-06-29 17:09:32 +0000

    update1

--- database.py
+++ database.py
@@ -1,13 +1,19 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+from datetime import datetime
+import os
+from dotenv import load_dotenv
 
+load_dotenv('.env')
 # Edit this with your path to serviceAccount.json after downloading from Firebase
-Certificate = r'C:\Users\hp\Desktop\feedpost using flask\serviceAccount.json'
- 
+
+Certificate = os.getenv('PATH_TO_SERVICE_ACCOUNT_JSON')
+
+
 cred = credentials.Certificate(Certificate)
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'YOUR_databaseURL '
+    'databaseURL': os.getenv('DATA_BASE_URL')
 })
 
 
@@ -20,7 +26,7 @@ def get_posts_data() -> dict :
     """
     ref = db.reference('/posts')
     posts : dict = ref.get()
-    
+    print('GPD -S ') #test
     return posts
 
 
@@ -49,8 +55,12 @@ def store_post_data(username : str , postText : str , time :str) -> None:
     postID   = f"{post_data['username'] }-{lastId}"
     postID_base = ref.child(postID)
     postID_base.set(post_data)
+    print('SPD - S') #test
     
 
 if __name__ == '__main__':
     #test of the store_post_data
-    store_post_data('john' , 'hello world' , '29/06/2024 02:51')
+    now : str = str(datetime.now())[0:16]
+    print(now)
+   # store_post_data('alfred' , 'hello world' , now)
+    get_posts_data()
